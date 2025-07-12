4be7c0ae6d2b3ce30c378850394cbf8ce358d689
mariami57 <mmitova3@gmail.com>
2025-07-12 14:29:13 +0300

    added more solutions

--- Django REST Basics/books_api/urls.py
+++ Django REST Basics/books_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls.conf import path, include
 
 from books_api import views
+from books_api.views import ListBookView
 
 urlpatterns = [
-
+    path('books/', ListBookView.as_view(), name='list-books'),
     path('book/', include([
     path('<int:pk>/', views.BookViewSet.as_view(), name='book'),
     ]))
--- Django REST Basics/books_api/views.py
+++ Django REST Basics/books_api/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from books_api.models import Book
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import views
+from rest_framework import status, views, generics
 from books_api.serializers import BookSerializer
 
 
@@ -23,35 +22,41 @@ from books_api.serializers import BookSerializer
 #     serializer = BookSerializer(book)
 #     return Response(serializer.data, status=status.HTTP_200_OK)
 
-class BookViewSet(views.APIView):
-    serializer = BookSerializer
+class ListBookView(generics.ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
-    def get_object(self, pk):
-        return get_object_or_404(Book, pk=pk)
+class BookViewSet(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
-    def get(self,request, pk):
-        book = self.get_object(pk)
-        serializer = self.serializer(book)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self,request, pk):
-        book = self.get_object(pk)
-        serializer = self.serializer(instance=book, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def get_object(self, pk):
+    #     return get_object_or_404(Book, pk=pk)
 
-    def patch(self,request,pk):
-        book = self.get_object(pk)
-        serializer = self.serializer(instance=book, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def delete(self,request,pk):
-        book = self.get_object(pk)
-        book.delete()
-        return Response(status=status.HTTP_200_OK)
+    # def get(self,request, pk):
+    #     book = self.get_object(pk)
+    #     serializer = self.serializer(book)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # def put(self,request, pk):
+    #     book = self.get_object(pk)
+    #     serializer = self.serializer(instance=book, data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # def patch(self,request,pk):
+    #     book = self.get_object(pk)
+    #     serializer = self.serializer(instance=book, data=request.data, partial=True)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    #
+    # def delete(self,request,pk):
+    #     book = self.get_object(pk)
+    #     book.delete()
+    #     return Response(status=status.HTTP_200_OK)
 
 
 #
